d778ddd8b86c08957a642f2aeb3d543746cfa3a9
Muhammad Riski <6538108-RizukiRetz@users.noreply.gitlab.com>
2020-10-15 15:56:52 +0800

    add event nav-link clicked

--- script.js
+++ script.js
@@ -9,3 +9,26 @@ window.onscroll = function() {
         navbar.classList.remove('active');
     }
 }
+
+const menuHam = document.querySelector('.menu-icon');
+const navLink = document.querySelector('.nav-link');
+// window.addEventListener = function() {
+//     menuHam.addEventListener('click',function(){
+
+//     })
+// }
+menuHam.addEventListener("click", toggleHamburger)
+function toggleHamburger(){
+  navLink.classList.toggle("close");
+  menuHam.classList.toggle("fa-times");
+  menuHam.classList.toggle("fa-bars");
+}
+
+function checkToggle(e){
+  var itemClicked = e.target
+  if (itemClicked.classList.contains("item-box")) {
+    toggleHamburger();
+  }
+}
+
+menuHam.addEventListener("click", checkToggle);
